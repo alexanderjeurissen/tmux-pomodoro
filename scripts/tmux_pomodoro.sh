4991d99cb26506fb8ca5b754c94180724a55de63
Alexander Jeurissen <alexander@jeurissen.email>
2017-11-28 01:57:59 +0100

    Fix pomodoro

--- scripts/tmux_pomodoro.sh
+++ scripts/tmux_pomodoro.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 source "$CURRENT_DIR/helpers.sh"
 
 _get_current_time_stamp() {
@@ -11,38 +12,38 @@ _display_progress() {
   local filled_glyph=''
   local empty_glyph=''
   local active_glyph=''
-  local block_duration=5 # duration in minutes per block
 
   local end_time="$(get_tmux_option "@pomodoro_end_at")"
   local current_time="$(_get_current_time_stamp)"
-  echo "end_time: $end_time, current_time: $current_time"
-  local time_remaining="$(( ( $end_time - $current_time )/(60*60) ))" # time remaining in minutes
-  local zero=0
+  local time_difference=$(( $end_time - $current_time ))
+  local time_remaining="$(( $time_difference / 60))" # time remaining in minutes
 
-  echo "time_remaining: $time_remaining"
-  if [[ $time_remaining -le $zero ]]; then
-    echo "☕POMO !"
+  local divider=5
+
+  local filled_segments=$(($time_remaining / $divider))
+  local empty_segments=$(($divider-$filled_segments))
+  local remainder=$(($time_remaining % $divider))
+
+  if (($time_remaining == 0)); then
+    pomodoro_stop
   else
-    local filled_segments=$(($time_remaining / $block_duration))
-    local empty_segments=$(($block_duration-$filled_segments))
-    local remainder=$(($time_remaining % $block_duration))
 
-    if [[ $remainder -gt $zero ]]; then
+    if (($remainder > 0)); then
       empty_segments=$(($empty_segments-1))
       active_segments="$active_glyph"
     fi
 
     filled_segments=`printf "%${filled_segments}s"`
     empty_segments=`printf "%${empty_segments}s"`
-    echo "${filled_segments// /$filled_glyph}$active_segments${empty_segments// /$empty_glyph}" |
+    echo "${filled_segments// /$filled_glyph}$active_segments${empty_segments// /$empty_glyph} $time_remaining" |
       sed 's/^[[:space:]]*//;s/[[:space:]]*$//'
   fi
 }
 
 pomodoro_start() {
-  echo "pomodoro started"
+  tmux display-message "⏰ POMODORO started"
   local current_time=$(_get_current_time_stamp)
-  local pomodoro_duration="$((25*60*60))" # duration is 25 minutes by default
+  local pomodoro_duration="$((25*60))" # duration is 25 minutes by default
   local end_time="$(( $current_time + $pomodoro_duration ))"
 
   set_tmux_option "@pomodoro_state" "active"
@@ -50,18 +51,17 @@ pomodoro_start() {
 }
 
 pomodoro_stop() {
-  echo "pomodoro stopped"
+  tmux display-message "☕ POMODORO stopped"
+  tmux clock-mode -t 1
   set_tmux_option "@pomodoro_state" "inactive"
+  set_tmux_option "@pomodoro_end_at" ""
 }
 
 pomodoro_status() {
-  echo "pomodoro status"
   local pomodoro_state="$(get_tmux_option "@pomodoro_state")"
 
   if [[ $pomodoro_state = "active" ]]; then
     echo "$(_display_progress)"
-  elif [[ $pomodoro_state = "inactive" ]]; then
-    echo "☕POMO !"
   fi
 }
 
--- tmux_pomodoro_status.tmux
+++ tmux_pomodoro_status.tmux
@@ -2,8 +2,11 @@
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-tmux_pomodoro="#($CURRENT_DIR/scripts/tmux_pomodoro.sh)"
+pomodoro_status="#($CURRENT_DIR/scripts/tmux_pomodoro.sh)"
 pomodoro_status_interpolation_string="\#{pomodoro_status}"
+
+tmux bind-key p run-shell "$CURRENT_DIR/scripts/tmux_pomodoro.sh start"
+tmux bind-key P run-shell "$CURRENT_DIR/scripts/tmux_pomodoro.sh stop"
 
 source $CURRENT_DIR/scripts/helpers.sh
 
@@ -18,6 +21,3 @@ main() {
   update_tmux_option "status-left"
 }
 main
-
-tmux bind-key p run-shell "$CURRENT_DIR/scripts/tmux_pomodoro.sh start"
-tmux bind-key P run-shell "$CURRENT_DIR/scripts/tmux_pomodoro.sh stop"
